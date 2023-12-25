fc9640e79cd3963fb0efaf46eb586eb6675426df
Nolen Johnson <johnsonnolen@gmail.com>
2023-12-25 14:37:01 -0500

    nx_tab: Initial tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_nx_tab.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_nx_tab-eng \
+    lineage_nx_tab-user \
+    lineage_nx_tab-userdebug
+
